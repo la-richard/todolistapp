20bd412a2f47a8af1e87c3766a2756d72e6628c6
richardpitargue <richard.pitargue@lawadvisor.com>
2024-01-24 16:29:02 +0800

    chore: remove unused files

--- test/todolistapp_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TodolistappWeb.PageControllerTest do
-  use TodolistappWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
