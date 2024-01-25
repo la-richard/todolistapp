16af97415281d9b92d08e92220806808eff421e4
richardpitargue <richard.pitargue@lawadvisor.com>
2024-01-25 19:00:19 +0800

    chore: center status icon in column

--- lib/todolistapp_web/components/core_components.ex
+++ lib/todolistapp_web/components/core_components.ex
@@ -493,7 +493,7 @@ defmodule TodolistappWeb.CoreComponents do
             <td
               :for={{col, i} <- Enum.with_index(@col)}
               phx-click={@row_click && @row_click.(row)}
-              class={["relative p-0" <> " " <> Map.get(col, :class, ""), @row_click && "hover:cursor-pointer"]}
+              class={["relative p-0", Map.get(col, :class, ""), @row_click && "hover:cursor-pointer"]}
             >
               <div class="block py-4 pr-6">
                 <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
